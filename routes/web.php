653f3dcd060083f9443814d5728aec586e2ff14b
Vlad Gruby <hello@sflwebdesign.com>
2017-03-09 10:40:16 -0500

    Added views for pages, base elements and partials: contact, about, footer, header, scripts, navigation. Updated web routes to match. Added static page controller. Updated home controller.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,10 +14,40 @@
 |
 */
 
+// Static Pages
+Route::get('/',              'StaticPageController@index');
+Route::get('about',          'StaticPageController@about');
+Route::get('pricing',        'StaticPageController@pricing');
+Route::get('contact',        'StaticPageController@contact');
+Route::get('terms-of-use',   'StaticPageController@termsOfUse');
+Route::get('privacy-policy', 'StaticPageController@privacyPolicy');
+Route::get('components',     'StaticPageController@components');
+
+/* Helper class that generates the routes required for user authentication
+
+    // Authentication Routes...
+        $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+        $this->post('login', 'Auth\LoginController@login');
+        $this->post('logout', 'Auth\LoginController@logout')->name('logout');
+    // Registration Routes...
+        $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+        $this->post('register', 'Auth\RegisterController@register');
+    // Password Reset Routes...
+        $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+        $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+        $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+        $this->post('password/reset', 'Auth\ResetPasswordController@reset');
+*/
+Auth::routes();
+
+// User and Admin controllers
+Route::get('admin', 'HomeController@index');
+Route::get('home/your', 'HomeController@index');
+
+// Other
+Route::get('home', 'HomeController@index');
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
