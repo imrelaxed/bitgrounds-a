1bcc9b240eebd99e9babdf083692262967379f7b
Vlad Gruby <hello@sflwebdesign.com>
2017-04-01 14:03:27 -0400

    Main dashboard UI finished.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="text-center">
-                    <h2>Hey <span class="text-primary"> {{ ucfirst(Auth::user()->name) }}</span></h2>
+                    <h2>Hi <span class="text-primary"> {{ ucfirst(Auth::user()->name) }}</span></h2>
 
                     @if( $is_subscribed )
 
@@ -30,7 +30,7 @@
                 <div class="panel-body gutter__padding">
 <div class="col-md-7 gutter__padding">
     <div class="row text-center control-panel__head">
-        <h4>General Info</h4>
+        <h4>Account Information</h4>
     </div>
     <div class="row control-panel__cell">
 
@@ -76,23 +76,24 @@
         <h4>Manage</h4>
     </div>
 <ul class="manage-bar">
-    <li><a href="#"><span class="menu-icon fa fa-home"></span> Direct Admin </a></li>
 
-    <li><a href="#"><span class="menu-icon fa fa-home"></span> Web Mail </a></li>
+    <li><a href="#"><span class="menu-icon fa fa-cogs"></span>Hosting Control </a></li>
+    <li><a href="#"><span class="menu-icon fa fa-unlock-alt"></span>Hosting Password </a></li>
+    <li><a href="#"><span class="menu-icon fa fa-envelope"></span>WebMail</a></li>
 
-    <li><a href="#"><span class="menu-icon fa fa-home"></span> Password Change </a></li>
+
 
 </ul>
 </div>
 
     <div class="col-xs-8 gutter__padding">
         <div class="row text-center control-panel__head">
-            <h4>Account Details</h4>
+            <h4>Important Information</h4>
         </div>
-        <a href="" class="btn-block btn btn-info"><span class="fa fa-home"></span>  Nameserver / DNS Details</a>
-        <a href="" class="btn-block btn btn-info"><span class="fa fa-home"></span>  Administration Details</a>
-        <a href="" class="btn-block btn btn-info"><span class="fa fa-home"></span>  File Upload Details</a>
-        <a href="" class="btn-block btn btn-info"><span class="fa fa-home"></span>  Email Setup Instructions</a>
+        <a href="" class="btn-block btn btn-info"><span class="fa fa-link"></span>  Nameserver / DNS Details</a>
+        <a href="" class="btn-block btn btn-info"><span class="fa fa-user-o"></span>  Administration Details</a>
+        <a href="" class="btn-block btn btn-info"><span class="fa fa-files-o"></span>  File Upload Details</a>
+        <a href="" class="btn-block btn btn-info"><span class="fa fa-envelope-o"></span>  Email Setup Instructions</a>
 
     </div>
 
